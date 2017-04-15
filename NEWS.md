8e608c31dbf368a005c6f8d9c120b1f826de7047
Lorenzo Busetto <lbusett@gmail.com>
2017-04-15 19:31:21 +0000

    version 1.3.2

---
+++ NEWS.md
@@ -0,0 +1,144 @@
+# MODIStsp v 1.3.2
+
+v1.3.2 was released on 22/03/2017
+
+## Major Changes:
+Added functionality to apply scale and offset coeeficients on MODIS original values according with the specifications of single MODIS products.
+
+### Details:
+NASA provides outputs as integer values, indicating a potential scale factor and/or an offset to apply in order to obtain values in the indicated measure units.
+- Leaving the "Scale output values" option to "No", output files are left as provided by NASA, and additional indices are produced as integer values with a 10000 factor scale; 
+- Setting the "Scale output values" option to "Yes", scale factor and offsets are applied if existing (for example, in this case Land Surface Temperature values in the output raster will be in °K), and  spectral indices are floating point values (for example, NDVI is between -1 and 1, etcetera).
+
+## Minor Changes:
+
+- Some product names and output layer names were modified to reduce the length of output file names, homogenize the names of the outputs and correct some errors.
+__For compatibility with already created output files__ (versions up to 1.3.1), the old "XML" file specifying output files format is still available in `inst/ExtData/MODIStsp_ProdOpts_old_v1.3.1.xml`. To use the old file naming conventions, users have to 
+1. delete `inst/ExtData/MODIStsp_ProdOpts.xml` and rename `MODIStsp_ProdOpts_old_v1.3.1.xml` to `MODIStsp_ProdOpts.xml`.
+2. delete `MODIStsp_ProdOpts.RData` from the `Previous` folder within `your_R-library_path/MODIStsp/Previous`
+3. Restart `MODIStsp`
+
+- Timeouts of httr/ftp requests were increased to prevent problems on download on slow connections
+
+## Bug fixing:
+
+- Fixed bug on FTP download speed (Issue [#65](https://github.com/lbusett/MODIStsp/issues/65)) 
+- Fixed bug on download of tile 0, preventing download of images with DOY 001 and of all "yearly based" products (e.g., MOD17)(Issue [#64](https://github.com/lbusett/MODIStsp/issues/64)) 
+- Fixed other bugs affecting FTP download (https://github.com/lbusett/MODIStsp/commit/efbf1b469e7518ffc8a7ec6d9922242d6a5c228f, https://github.com/lbusett/MODIStsp/commit/1dc53a5ff5b355965acec86678a3104bd2d27fd9, https://github.com/lbusett/MODIStsp/commit/fa6c7b42eadce516a2f781604c9db28418120f36)
+
+
+
+# MODIStsp v 1.3.1
+
+v1.3.1 was released on 13/02/2017
+
+## Major Changes
+
+- Added functionality for processing of Snow Cover datasets: MOD10A1, MOD10A2, MOD10C1, MOD10C2, MOD10CM (Issue
+[#55](https://github.com/lbusett/MODIStsp/issues/55)) on devel
+
+- Added functionality for downloading "partial" years(Issue [#54](https://github.com/lbusett/MODIStsp/issues/54)) on devel
+
+- Added functionality for computing vegetation indexes on MCD43A4 (v5-v6), MCD43B4 (v5), MCD43C4 (v5-v6) (Issue [#59](https://github.com/lbusett/MODIStsp/issues/59)) on master/devel
+
+- Added functionality for accelerating download using aria2c (Issue [#55](https://github.com/lbusett/MODIStsp/issues/55)) on devel
+
+## Bug fixing
+
+- Fixed bug on download with aria, throwing an error on partial download on http downlaod with aria ([6fbc875](https://github.com/lbusett/MODIStsp/commit/6fbc87547b6214b500afc0291c02166c0b855c78))
+
+- Fixed bug on M*D15A2 processing (Issue [#60](https://github.com/lbusett/MODIStsp/issues/60)) on devel/master
+
+- Fixed bug on MCD12Q1 processing (Issue [#58](https://github.com/lbusett/MODIStsp/issues/58)) on devel/master
+
+- Fixed bug on MOD13C2 processing (Issue [#52](https://github.com/lbusett/MODIStsp/issues/52)) on devel/master
+
+- Fixed bug on insertion of custom projection (Issue [#57](https://github.com/lbusett/MODIStsp/issues/57)) on devel/master
+
+- Fixed bug on selection of custom index (Issue [#53](https://github.com/lbusett/MODIStsp/issues/53)) on devel/master
+
+
+# MODIStsp v 1.3.0  Release NOtes
+
+v1.3.0 was released on 11/05/2016
+
+## Major Changes
+
+1. Added functionality for downloading and preprocessing MODIS collection 006 datasets. For products with both 005 and 006 collections, the user can select the version using a new droplist in the GUI.
+
+2. Added functionality for off-line processing. This allows both to _i)_ reprocessing already downloaded data (for example, to create time series for an additional layer) without the need to connect to NASA servers, and _ii)_ process HDF files downloaded outside _MODIStsp_ (e.g., directly from NASA ftp) and stored on the user's PC, without the need of an active internet connection. 
+
+3. Improved the way in which options are saved. Much more readable .JSON files are now used instead than .RData. User options are no longer saved alongside products characteristics. This will allow to re-use an "old" options file even if changes are made on the XML file descriving the products.
+
+4. Improved the GUI inteface for specifying additional Spectral Indexes. Hints are now showed to the user, and multiple indexes can be added in the same session.
+
+## Minor Changes
+
+1. General improvements in the GUI inteface. Products are now grouped by categories, to allow easier identification and selection.
+
+2. Improvements in the README file and vignettes, providing more instructions on package use.
+
+3. Improved functionality for checking for "complete" download, by comparing the size of the downloaded files with that of files on the server. 
+
+4. Added "configure" file for Linux installation.
+
+4. Temporary files necessary for processing (e.g., vrt files) are now created (and destroyed) within the "R" temporary folder.
+
+5. Miscellaneous bug-fixing.
+
+
+# MODIStsp v 1.2.1 Release NOtes
+
+v1.2.1 was released on 04/20/2016
+ 
+## Major Changes
+
+1. Modified format of "R" output time series from _rts_ objects to _RasterStack_ objects with temporal information added in the 
+"z" attribute via setZ()
+
+2. Major changes/improvements in _MODIStsp\_extract_ function:
+    * Use of plain rasterstack with "z" attribute instead than rasterstackts
+    * Use of gdal\_rasterize (_gdalUtils_) instead of rasterize (_rgdal_) to improve speed. Temporary shapes and rasters necessay are saved in "R" temporary folder and removed automatically
+    * Fixed bugs on functionality for point/lines shapefiles, according to what specified by the "small" and "small_method" parameters
+    * Added functionality for retrieving data for small polygons
+    * Added out_format selection - xts or plain data.frame
+    * Added possibility to use a shp filename as input directly
+    * Added conformity checks on inputs
+    * Added functionaluity to run without specifying start and end dates
+    * Added id_field parameter for choosing which column of the input SP object should be used for "naming" the columns of the output
+  
+3. Removed possibility to use "complex" resampling methods when reprojecting (e.g., bilinear, cubic, etc.) to avoid incorrect resampling on categorical variables and "contamination" of good pixels data. 
+
+## Minor Changes
+
+* Changed the input method for starting and ending dates selection in the GUI. Now a text field is used 
+* Added functionaluty for writing data ignore value on ENVI files
+* Removed automatic deletion of XML files created by writeRaster to keep metadata information
+* Changed names of products in the GUI for products with both TERRA and AQUA dataset to M\*D09A1, M\*D13Q1, etc...
+* Modified code syntax to satisfy R code styling guidelines
+* Modified roxygen parameters so that only required functions are imported from dependent packages
+* Updated and corrected the list of dependencies
+* Updated required "R" version to 3.2, and minimum versions for dependent packages to current versions.
+* Added Welcome message
+* Updated links to LPDAAC product description pages
+* Changed all "print" and "cat" calls to show messages/warnings to "message" or "warning" to allow easy disabling MODIStsp verbose messages
+* Using "R" tempfile/tempdir to save vrt files
+
+## Bug Fixes
+
+* Corrected a bug that threw an error in case incorrect bounding box specified
+
+# MODIStsp v 1.2.0 Release Notes
+
+v1.2.0 was released on 29/07/2015
+
+
+## Major changes
+
+First stable release of advanced implementation of MODIStsp !
+We know it should be 1.0.0, but thats'it !
+
+
+
+
+
---
+++ R/MODIStsp_extract.R
@@ -0,0 +1,304 @@
+#' MODIStsp_extract
+#' @description
+#' function used to extract time series data from rts files created by MODIStsp
+#' on spatial locations provided in the form of "R" spatial objects (spatialPoints,
+#' spatialPolygons, etc.)
+#' @details
+#' The function takes as input a rasterStack object containing time information in the "z" attribute (set by "raster" function "SetZ"),
+#' a starting and ending date  and a standard "R" spatial object, and returns the time series for the spatial locations
+#' specified in the spatial object in the form of a "R" xts object ORr a plain data.frame with a "date" column in first position.
+#' If the input spatial object is a "point" or "line" one, the  output object contains one column for each specified point, or for each cell
+#' intersecting the line, and one line for each date. If the input spatial object is a "polygon" one, the output object contains one column
+#' for each polygon, containing values obtained applying the function specified as the FUN
+#' argument over all pixels belonging to the polygon, and one line for each date.
+#'
+#' @param in_rts input "rasterStack" or "rasterBrick" object created by MODIStsp (it MUST contain acquisition dates in the "Z" attribute " )
+#' @param sp_object "sp" object OR name of an ESRI shapefile specifying the "positions" from which data has to be extracted
+#' 	If sp_object represents lines, the output object contains one column for each polygon, containing values obtained applying the function
+#'  specified as the FUN argument over all pixels touched by the line, and one line for each date.
+#'  If sp_object represents points, the output object contains one column for each point, containing values of the cells corresponding to the pint,
+#'  and one line for each date.
+#'  If sp_object represents polygons, the output object contains one column for each polygon, containing values obtained applying the function
+#'  specified as the FUN argument over all pixels belonging to the polygon, and one line for each date
+#' @param start_date "Date", "POSIXct" or "POSIXlt" starting date of the period to be considered for data extraction OR character string cohercible to
+#' Date class (format = "yyyy-mm-dd"). If not provided, the starting date of the rasterstack is used.
+#' @param end_date "Date", "POSIXct" or "POSIXlt" ending date of the period to be considered for data extraction OR character string cohercible to
+#' Date class (format = "yyyy-mm-dd").If not provided, the ending date of the rasterstack is used.
+#' @param id_field character name of the column of the input sp object or shapefile to be used in the data extraction. Values conatined in the column MUST be unique
+#' The names of the columns of the output are taken from this column. If not provided, or an invalid value is provided, then the names of the columns of the output
+#' reflect the number of the feature in the original sp object or shapefile.
+#' @param FUN function to summarize the values (e.g. mean) on polygon data frames. The function should take a single numeric vector as argument and return a
+#' single value (e.g. mean, min or max), and accept a na.rm argument. Thus, standard R functions not including an na.rm argument must
+#' be wrapped as in this example: fun=function(x,...)length(x). Defaults to "mean"
+#' @param out_format character "xts" or "dframe". default to 'xts'. If dframe, the output is a data frame with dates in the first column and extracted data in the others
+#' @param small logical If set to TRUE, and input is polygons, then values are returned also for polygons not covering at least one raster cell. "Included"
+#' cells in this case depend on the values of the "small_method" parameter.
+#' @param small_method string 'centroids' or 'full'. if small == T and input is polygons, controls which cells are "extracted" for small polygons. If set to
+#' centroids (default), then only the cells corresponding to polygon centroid are considered (faster, may have problems on strangely shaped polygons). If set to
+#' "full", then all cells intersected by the small polygon are extracted and used in calculation.
+#' @param na.rm	Logical If TRUE, and sp_object is a polygon, then na.rm = T is used when applying the function
+#' 	to the different pixels of the polygon. Default = T.
+#' @param verbose Logical If TRUE, messages on extraction completion are sent out. Default = T.
+#'
+#' @return data.frame or xts object. Each column of data corresponds to one point or one polygon
+#'
+#' @author Lorenzo Busetto, phD (2015)
+#' email: busetto.l@@irea.cnr.it
+#'
+#' @note License: GPL 3.0
+#' @export
+#' @importFrom rgdal readOGR writeOGR
+#' @importFrom xts as.xts
+#' @importFrom data.table data.table setkey
+#' @importFrom raster getValues crop extent getZ extract rasterize
+#' @importFrom sp coordinates CRS proj4string spTransform
+#' @importFrom tools file_path_sans_ext
+#' @importFrom gdalUtils gdal_rasterize
+#' @importFrom raster res
+#' @examples
+#' # Extract average and standard deviation values from a rts object created by MODIStsp
+#' # for each polygon of a shapefile, for each date in the period between 2001-01-01 and 2014-12-31
+#' \dontrun{
+#' #Set the inputs
+#' infile <- "in_path/MOD13Q1_MYD13Q1_NDVI_49_2000_353_2015_RData.RData"  # Input rts file
+#' shpname <- "path_to_file/rois.shp"  # Path to Polygon Shapefile
+#' startdate <- as.Date("2010-01-01")  # Start date for extraction
+#' enddate <- as.Date("2014-12-31")    # End date for extraction
+#' #Load Data
+#' inrts <- get(load(infile))          # Load the rts file
+#' # Compute average and St.dev
+#' dataavg <- MODIStsp_extract(inrts,shpname, startdate, enddate, FUN = 'mean', na.rm = T)
+#' datasd <- MODIStsp_extract(inrts,shpname,  startdate,  enddate,  FUN = 'sd', na.rm = T)
+#' plot(dataavg)
+#' }
+
+
+MODIStsp_extract <- function (in_rts, sp_object, start_date = NULL, end_date = NULL,
+                              id_field = NULL, FUN = "mean", out_format = "xts", small = TRUE,
+                              small_method = "centroids", na.rm = TRUE, verbose = FALSE) {
+  if (!class(in_rts) %in% c("RasterStack", "RasterBrick")) {
+    stop("Input is not a RasterStack or RasterBrick object")
+  }
+  if (!class(getZ(in_rts)) == "Date") {
+    stop("Input doesn't contain valid dates in its 'Z' attribute !")
+  }
+  if (length(start_date) == 0) {
+    start_date <- min(getZ(in_rts))
+    if (verbose)
+      message("Starting date not provided - Using the first date in the stack")
+  }
+  if (length(end_date) == 0) {
+    end_date <- max(getZ(in_rts))
+    if (verbose)
+      message("Ending date not provided - Using the last date in the stack")
+  }
+  if (!class(start_date) %in% c("Date", "POSIXct", "POSIXlt")) {
+    start_date <- try(as.Date(start_date), silent = TRUE)
+    if (class(start_date) == "try-error") {
+      stop("start_date is not a Date object or string cohercible to date")
+    }
+  }
+  if (!class(end_date) %in% c("Date", "POSIXct", "POSIXlt")) {
+    end_date <- try(as.Date(end_date), silent = TRUE)
+    if (class(end_date) == "try-error") {
+      stop("end_date is not a Date object or string cohercible to date")
+    }
+  }
+  if (start_date > end_date) {
+    stop("start_date larger than end_date")
+  }
+  if (!small_method %in% c("centroids", "full")) {
+    warning("Unknown 'small_method' value - resetting to 'centroids'")
+  }
+  if (!out_format %in% c("xts", "dframe")) {
+    if (verbose)
+      message("Unknown 'out_format' value - resetting to 'xts'")
+    out_format <- "xts"
+  }
+  if (!class(sp_object) %in% c("SpatialPolygonsDataFrame",
+                               "SpatialPolygons", "SpatialPointsDataFrame", 
+                               "SpatialPoints",
+                               "SpatialLines", "SpatialLinesDataFrame")) {
+    if (class(sp_object) == "character") {
+      sp_object <- try(readOGR(dirname(sp_object), basename(file_path_sans_ext(sp_object))))
+      if (class(sp_object) == "try-error") {
+        stop("sp_object is not a valid Spatial object or Shapefile")
+      }
+    }
+  }
+  if (length(id_field) != 0) {
+    if (!id_field %in% names(sp_object)) {
+      warning("Invalid 'id_field' value - names of output columns will be the record 
+              number of the shapefile feature")
+      id_field <- NULL
+    }
+  }
+  dates <- getZ(in_rts)
+  sel_dates <- which(dates >= start_date & dates <= end_date)
+  
+  if (length(sel_dates) > 0) {
+    if (proj4string(sp_object) != proj4string(in_rts)) {
+      sp_object <- spTransform(sp_object, CRS(proj4string(in_rts[[1]])))
+    }
+    sp_object@data$mdxtnq <- seq(1:length(sp_object@data[, 1]))
+    shape <- crop(sp_object, extent(in_rts[[1]]))
+    if (!isTRUE(all.equal(extent(shape), (extent(sp_object)), scale = 100))) {
+      warning("Some features of the spatial object are outside or partially outside\n 
+              the extent of the input RasterStack ! Output for features outside rasterstack 
+              extent\n will be set to NODATA. Outputs for features only partially inside will
+              be retrieved\n using only the available pixels !")
+      if (!setequal(sp_object$mdxtnq, shape$mdxtnq)){
+        
+        outside_feat <- setdiff(sp_object$mdxtnq, shape$mdxtnq)
+      }
+    }
+    if (class(shape) %in% c("SpatialPointsDataFrame", "SpatialPoints",
+                            "SpatialLines", "SpatialLinesDataFrame")) {
+      
+      ts <- matrix(nrow = length(sel_dates), ncol = length(shape[, 1]))
+      for (f in 1:length(sel_dates)) {
+        if (verbose == TRUE) {
+          print(paste0("Extracting data from date: ",
+                       dates[sel_dates[f]]))
+        }
+        ts[f, ] <- extract(in_rts[[sel_dates[f]]], shape,
+                           fun = FUN)
+      }
+      ts <- as.data.frame(ts)
+      if (length(id_field) == 1) {
+        feat_names <- as.character(shape@data[, eval(id_field)])
+        names(ts) <- c(feat_names)
+      } else {
+        names(ts) <- 1:length(shape[, 1])
+      }
+      
+      if (out_format == "dframe") {
+        ts <- cbind(date = dates[sel_dates], ts)
+      }
+    # On polygons, extract by rasterization !
+    } else {
+      if (verbose)
+        (message("Rasterizing shape"))
+      if (verbose) {
+        message("Writing temporary shapefile")
+      }
+      tempshape <- tempfile(tmpdir = tempdir(), fileext = ".shp")
+      writeOGR(shape, dsn = dirname(tempshape), layer = basename(file_path_sans_ext(tempshape)),
+               driver = "ESRI Shapefile", overwrite_layer = TRUE,
+               verbose = FALSE)
+      if (verbose) {
+        message("Writing temporary rasterized shapefile")
+      }
+      tempraster <- tempfile(tmpdir = tempdir(), fileext = ".tiff")
+      ext_conv <- function(x) {
+        ext <- extent(x)
+        c(ext[1], ext[3], ext[2], ext[4])
+      }
+      if (max(shape@data$mdxtnq) <= 255) {
+        ot <- "Byte"
+      }       else {
+        if (max(shape@data$mdxtnq) <= 65536) {
+          ot <- "Int16"
+        }
+        else {
+          ot <- "Int32"
+        }
+      }
+      gdal_rasterize(tempshape, tempraster, tr = raster::res(in_rts),
+                     te = ext_conv(in_rts[[1]]), a = "mdxtnq", ot = ot)
+      zone_raster <- raster(tempraster)
+      zones <- getValues(zone_raster)
+      ok_zones <- which(is.finite(zones) & zones != 0)
+      zones <- zones[ok_zones]
+      ncols <- length(unique(zones))
+      ts <- matrix(nrow = length(sel_dates), ncol = ncols)
+      
+      for (f in 1:length(sel_dates)) {
+        if (verbose == TRUE) {
+          message(paste0("Extracting data from date: ", dates[sel_dates[f]]))
+          
+        }
+        
+        value <- getValues(in_rts[[sel_dates[f]]])[ok_zones]
+        rDT <- data.table(value, zones)
+        setkey(rDT, zones)
+        .SD <- NULL # Workaround to avoid note on package check
+        ts[f, 1:ncols] <- rDT[, lapply(.SD, match.fun(FUN), na.rm = na.rm), by = zones]$value
+        
+      }
+      ts <- as.data.frame(ts)
+      if (length(id_field) == 1) {
+        feat_names <- as.character(sp_object@data[, eval(id_field)])[sort(unique(zones))]
+        
+        names(ts) <- feat_names
+      }
+      else {
+        feat_names <- as.character(shape@data[, "mdxtnq"])[sort(unique(zones))]
+        names(ts) <- feat_names
+      }
+      if (out_format == "dframe") {
+        ts <- cbind(date = dates[sel_dates], ts)
+      }
+      
+      if (small & ncols != length(shape@data[, 1])) {
+        if (length(id_field) == 1) {
+          miss_feat <- setdiff(as.character(shape@data[, "mdxtnq"]), names(ts))
+          pos_missing <- which(as.character(shape@data[, "mdxtnq"]) %in% miss_feat)
+        } else {
+          pos_missing <- miss_feat <- which(as.character(shape@data[, "mdxtnq"]) %in% miss_feat)
+        }
+        
+        shpsub <- shape[pos_missing, ]
+        ts_mis <- matrix(nrow = length(sel_dates), ncol = length(pos_missing))
+        for (f in 1:length(sel_dates)) {
+          if (verbose == TRUE) {
+            print(paste0("Extracting data from date: ", dates[sel_dates[f]]))
+            
+          }
+          if (small_method == "centroids") {
+            ts_mis[f, ] <- extract(in_rts[[sel_dates[f]]], coordinates(shpsub), fun = mean)
+            
+          } else {
+            ts_mis[f, ] <- extract(in_rts[[sel_dates[f]]], shpsub, fun = mean)
+            
+          }
+          
+        }
+        colnames(ts_mis) <- miss_feat
+        ts <- cbind(ts, ts_mis)
+      }
+      file.remove(tempraster)
+      file.remove(tempshape)
+    }
+    
+    
+    if (exists("outside_feat")) {
+      if (length(id_field) == 1) {
+        feat_names_outside <- as.character(sp_object@data[, eval(id_field)])[outside_feat]
+        
+      } else {
+        feat_names_outside <- as.character(sp_object@data[, "mdxtnq"])[outside_feat]
+        
+      }
+      
+      ts_outside <- matrix(nrow = length(sel_dates), ncol = length(feat_names_outside))
+      ts_outside <- data.frame(ts_outside)
+      names(ts_outside) <- feat_names_outside
+      ts <- cbind(ts, ts_outside)
+      if (length(id_field) == 1) {
+        sortindex <- match(sp_object@data[, eval(id_field)], names(ts))
+      } else {
+        sortindex <- match(sp_object@data[, "mdxtnq"], names(ts))
+      }
+      ts <- ts[, c(1, sortindex)]
+    }
+    if (out_format == "xts") {
+      ts <- as.xts(ts, order.by = dates[sel_dates])
+    }
+    return(ts)
+  } else {
+    warning("Selected time range does not overlap with the one of the rasterstack 
+            input dataset !")
+  }
+  
+}
